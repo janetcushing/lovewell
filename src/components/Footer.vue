aa745565f4f74261d427bad3d6aca120ae3ae285
Janet Cushing <janet.cushing@gmail.com>
2025-03-12 13:44:00 -0400

    fix privacy policy button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="container text-center footer">
     <div class="row align-items-end">
       <div class="col-md-1"></div>
-      <div class="col-md-1 justify-content-center">
+      <div class="col-md-2">
         <a
           role="button"
           id="ppbtn"
@@ -13,7 +13,7 @@
           Privacy Policy
         </a>
       </div>
-      <div id="footer-text" class="col-md-8 justify-content-center">
+      <div id="footer-text" class="col-md-6 justify-content-center">
         <p>&copy;Lovewell Pond Association - All rights reserved.</p>
         <p>Property Owners of Lovewell Pond Association</p>
         <p>P.O. Box 617, Fryeburg, ME 04037</p>
@@ -30,7 +30,7 @@
           >
         </p>
       </div>
-      <div class="col-md-1 justify-content-center">
+      <div class="col-md-2 justify-content-center">
         <a href="https://www.wunderground.com/?cm_ven=cgi" target="_blank"
           >Fryeburg Weather
           <img
@@ -61,6 +61,10 @@ export default {
   padding: 10px;
   font: 14px/10px Ubuntu;
   font-family: "Ubuntu", "Source Sans Pro", sans-serif;
+}
+#ppbtn {
+  padding: 30px 30px 30px 30px;
+  color: white;
 }
 
 .footer a {
